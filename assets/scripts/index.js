cb0da641098222c677183bafc8f413ed46205331
alex-braun <braunacb@gmail.com>
2016-09-11 22:13:10 -0400

    OAuth displaying my birthdate.
    Birthdate response.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,33 @@
 'use strict';
 
 // user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+// let example = require('./example');
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+//https://developers.google.com/api-client-library/javascript/samples/samples#LoadinganAPIandMakingaRequest
+// function appendResults(text) {
+//         let results = document.getElementById('results');
+//         results.appendChild(document.createElement('P'));
+//         results.appendChild(document.createTextNode(text));
+//       }
+//
+// function makeRequest() {
+//   let request = gapi.client.urlshortener.url.get({
+//     'shortUrl': 'http://goo.gl/fbsS'
+//   });
+//
+//   request.then(function(response) {
+//     appendResults(response.result.longUrl);
+//   }, function(reason) {
+//     console.log('Error: ' + reason.result.error.message);
+//   });
+// }
+//
+// function init() {
+//   gapi.client.setApiKey('AIzaSyDWFafdDL7lofPzZPLY3RuTmacbQ3W13jk');
+//   gapi.client.load('urlshortener', 'v1').then(makeRequest);
+// }
+//
+// gapi.load('client', init);
